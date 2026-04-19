36ed9f2334233de6c097953988468da6809c9c8a
Pedro N <itiropedro@gmail.com>
2026-04-19 17:18:39 -0300

    sync

--- src-flakyrepair/main/java/fr/inria/astor/approaches/flakyrepair/FlakyRepairEngine.java
+++ src-flakyrepair/main/java/fr/inria/astor/approaches/flakyrepair/FlakyRepairEngine.java
@@ -37,7 +37,6 @@ public class FlakyRepairEngine extends jMutRepairExhaustive {
          * Processing patches by re-executing them.
          */
         ConfigurationProperties.setProperty(ExtensionPoints.VALIDATION.identifier, FrProcessValidator.class.getCanonicalName());
-        ConfigurationProperties.setProperty("canhavezerosusp", Boolean.TRUE.toString());
         ConfigurationProperties.setProperty("includeTestInSusp", Boolean.TRUE.toString());
 
         /*
